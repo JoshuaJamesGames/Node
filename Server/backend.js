c0005664856bb72813e7802b4e475ace3f029eb8
Joshua James <thecoloradothompsons@gmail.com>
2023-12-20 21:44:43 -0700

    Adding players to backend and client

--- Server/backend.js
+++ Server/backend.js
@@ -3,16 +3,35 @@ const app = express();
 
 const http = require('http');
 const server = http.createServer(app);
-
+const {Server} = require('socket.io');
+const io = new Server(server);
 const port = 5500;
+const hostname = '127.0.0.1';
 
-app.use(express.static('Client'))
+app.use(express.static('Client'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname +'/index.html')
-})
-const hostname = '127.0.0.1';
+  res.sendFile(__dirname +'/index.html');
+});
+
+const backendPlayers = {};
+io.on('connection', (socket) =>{
+  console.log('A Player has Connected');
+  backendPlayers[socket.id] = {
+    x: 0,
+    y: 1.6,
+    z: 3
+  };
+
+  io.emit('updatePlayers', backendPlayers);
+
+  console.log(backendPlayers);
+});
+
 
 server.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
+
